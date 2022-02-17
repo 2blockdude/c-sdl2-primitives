508cfc6cbd8ce7e964f53683890eaf6efa8b0de5
Anthony Cobb <2blockdude@gmail.com>
2022-02-16 21:53:16 -0500

    spaces

--- primitives.h
+++ primitives.h
@@ -55,23 +55,23 @@ struct ellipse
 };
 
 // build float polygons
-struct polygon *create_polygon			(float *input_vectors, int nsides, float x, float y, float angle);
-struct polygon *create_reg_polygon	   (int nsides, float x, float y, float angle, float radius);
-struct polygon *create_rand_polygon	   (int nsides, float x, float y, float angle, float max_radius, float min_radius, float angle_offset);
+struct polygon *create_polygon         (float *input_vectors, int nsides, float x, float y, float angle);
+struct polygon *create_reg_polygon     (int nsides, float x, float y, float angle, float radius);
+struct polygon *create_rand_polygon    (int nsides, float x, float y, float angle, float max_radius, float min_radius, float angle_offset);
 struct polygon *create_copy_polygon    (struct polygon *p);
 
 // transform float polygon
-int polygon_rebuild							(struct polygon *p);
+int polygon_rebuild                    (struct polygon *p);
 
-int polygon_translate						(struct polygon *p, float x, float y);
+int polygon_translate                  (struct polygon *p, float x, float y);
 int polygon_rotate                     (struct polygon *p, float angle);
 int polygon_scale                      (struct polygon *p, float scale_x, float scale_y);
 
 int polygon_set_pos                    (struct polygon *p, float x, float y);
-int polygon_set_angle						(struct polygon *p, float angle);
-int polygon_set_scale						(struct polygon *p, float scale_x, float scale_y);
+int polygon_set_angle                  (struct polygon *p, float angle);
+int polygon_set_scale                  (struct polygon *p, float scale_x, float scale_y);
 
 // free float polygon
-void free_polygon							   (struct polygon *p);
+void free_polygon                      (struct polygon *p);
 
 #endif
